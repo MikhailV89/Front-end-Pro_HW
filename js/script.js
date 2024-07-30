b352a6ce2631ce4da2bdc014b3cfd5eee2badb8e
https://github.com/MikhailV89 <mvasilchenko89@gmail.com>
2024-07-30 22:01:51 +0300

    hw2 ready

--- js/script.js
+++ js/script.js
@@ -1,2 +1,22 @@
 'use strict';
+// Task 1
+
+const name = 'John';
+const admin = name;
+
+alert(admin);
+
+// Task 2
+
+const earthName = 'Earth';
+const websiteVisitor = 'some name'
+
+// Task 3
+
+const value = 10 + '10';
+console.log(value);
+
+// Получим строку '1010', т.к оператор + , если одно из значений строка, а второе число, преобразует число
+// в строку и произойдет конкатенация строк.
+
 
