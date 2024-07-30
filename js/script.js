941ddba6d7eeea320e206948024fab3a041b42fb
https://github.com/MikhailV89 <mvasilchenko89@gmail.com>
2024-07-30 22:09:53 +0300

    hw3 ready

--- js/script.js
+++ js/script.js
@@ -1,2 +1,17 @@
 'use strict';
 
+const firstValue = +prompt('Write first value');
+const secondValue = +prompt('Write second value');
+
+const sum = firstValue + secondValue;
+console.log(sum);
+
+const minus = firstValue - secondValue;
+console.log(minus);
+
+const multiply = firstValue * secondValue;
+console.log(multiply);
+
+const divide = firstValue / secondValue;
+console.log(divide);
+
