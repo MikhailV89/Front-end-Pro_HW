8cc503bf5379e3a8a3eb2b4d1ac691dfd166dfc3
https://github.com/MikhailV89 <mvasilchenko89@gmail.com>
2024-08-02 22:13:17 +0300

    hw4 ready

--- js/script.js
+++ js/script.js
@@ -1,2 +1,20 @@
 'use strict';
 
+const name = prompt('What is your name?');
+const age = prompt('What is your age ?');
+const likesProgramming = confirm('Do you like programming?');
+
+console.log(name, typeof name);
+console.log(age, typeof age);
+console.log(likesProgramming, typeof likesProgramming);
+
+const greeting = `Hello,${name}`;
+alert(greeting);
+
+const numericAge = Number(age);
+const nextAge = numericAge + 1;
+console.log(nextAge);
+
+const isAdult = nextAge >= 18;
+console.log(isAdult);
+
