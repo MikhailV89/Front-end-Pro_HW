bf66d1ce1aa7d726154d6014590b9e063e170350
https://github.com/MikhailV89 <mvasilchenko89@gmail.com>
2024-08-25 18:28:02 +0300

    hw12 ready

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,31 @@
 'use strict';
 
-const num1 = +prompt('Write first number');
-const num2 = +prompt('Write second number');
-const num3 = +prompt('Write third number');
+function padString(str, numLength, symbol = '@', symRight = true) {
+  if (typeof str !== 'string') {
+    return 'Error: The first argument must be a string.';
+  }
+  if (typeof numLength !== 'number') {
+    return 'Error: The second argument must be a number.';
+  }
+  if (typeof symbol !== 'string' || symbol.length !== 1) {
+    return 'Error: The third argument must be a string with a length of 1.';
+  }
+  if (typeof symRight !== 'boolean') {
+    return 'Error: The fourth argument must be a boolean.';
+  }
 
-const result = (num1 + num2 + num3) / 3;
+  if (numLength < str.length) {
+    return str.substr(0, numLength);
+  }
 
-alert(`Cереднє арифметичне трьох чисел: ${result}`);
+  const padLength = numLength - str.length;
+  const symRepeat = symbol.repeat(padLength);
+
+  const result = symRight ? str + symRepeat : symRepeat + str;
+
+  return result;
+}
+
+console.log(padString('hello', 8, '*'));
+console.log(padString('hello', 6, '*', false));
+console.log(padString('hello', 2));
