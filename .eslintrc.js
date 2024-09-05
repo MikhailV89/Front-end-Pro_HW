54ea0f34597f9c207390b5b066db94ea79e8e042
https://github.com/MikhailV89 <mvasilchenko89@gmail.com>
2024-09-05 19:01:00 +0300

    hw18 ready

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,15 @@
 module.exports = {
-  extends: ['airbnb'],
-  rules: {
-    strict: 'off',
+  'extends': ['airbnb'],
+  'rules': {
+    'strict': 'off',
     'no-undef': 'off',
     'no-alert': 'off',
-    'no-console': 'off'
+    'no-console': 'off',
+    'no-plusplus': 'off',
+    'prefer-template': 'off',
+    'no-unused-vars': 'off',
+    'no-unreachable': 'off',
+    'no-param-reassign': 'off',
+    'quote-props': ['error', 'always'],
   },
-}
+};
