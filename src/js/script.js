6b0ff2dee914f135385916bd9f6f1d3b9f42d8c7
https://github.com/MikhailV89 <mvasilchenko89@gmail.com>
2024-09-09 15:11:54 +0300

    hw17 ready

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,93 @@
 'use strict';
 
-const num1 = +prompt('Write first number');
-const num2 = +prompt('Write second number');
-const num3 = +prompt('Write third number');
+const arr = [16, -37, 54, -4, 72, -56, 47, 4, -16, 25, -37, 46, 4, -51, 27, -63, 4, -54, 76, -4, 12, -35, 4, 47];
 
-const result = (num1 + num2 + num3) / 3;
+// 1
+const numArr = (array) => {
+  const positive = [];
+  let positiveCount = 0;
 
-alert(`Cереднє арифметичне трьох чисел: ${result}`);
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > 0) {
+      positive.push(array[i]);
+    }
+  }
+
+  positive.sort((a, b) => a - b);
+
+  for (let i = 0; i < positive.length; i++) {
+    positiveCount += positive[i];
+  }
+
+  console.log(positive);
+  console.log(positiveCount);
+};
+
+numArr(arr);
+
+// 2
+const minElement = (array) => {
+  const minElementInArr = Math.min(...array);
+  const elementIndex = array.indexOf(minElementInArr);
+
+  console.log(`Element: ${minElementInArr}/ Index: ${elementIndex}`);
+};
+
+minElement(arr);
+
+// 3
+const maxElement = (array) => {
+  const maxElementInArr = Math.max(...array);
+  const elementIndex = array.indexOf(maxElementInArr);
+
+  console.log(`Element: ${maxElementInArr}/ Index: ${elementIndex}`);
+};
+
+maxElement(arr);
+
+// 4
+const someArr = (array) => {
+  const filtered = array.filter((num) => num < 0).length;
+  console.log(filtered);
+};
+someArr(arr);
+
+// 5
+const countPositiveEl1 = (array) => {
+  const result = array.filter((num) => num > 0 && num % 2 !== 0).length;
+  console.log(`Result ${result}`);
+};
+countPositiveEl1(arr);
+// 6
+const countPositiveEl2 = (array) => {
+  const result = array.filter((num) => num > 0 && num % 2 === 0).length;
+  console.log(`Result ${result}`);
+};
+countPositiveEl2(arr);
+// 7
+const sumPositiveEl1 = (array) => {
+  const result = array.filter((num) => num > 0 && num % 2 === 0)
+    .reduce((acc, num) => acc + num, 0);
+  console.log(result);
+};
+sumPositiveEl1(arr);
+// 8
+const sumPositiveEl2 = (array) => {
+  const result = array.filter((num) => num > 0 && num % 2 !== 0)
+    .reduce((acc, num) => acc + num, 0);
+  console.log(result);
+};
+sumPositiveEl2(arr);
+// 9
+const productOfPositiveEl = (array) => {
+  const result = array.filter((num) => num > 0).reduce((acc, num) => acc * num, 1);
+  console.log(result);
+};
+productOfPositiveEl(arr);
+// 10
+const onlyMaxElement = (array) => {
+  const max = Math.max(...array);
+  const result = arr.map((num) => (num === max ? max : 0));
+  console.log(result);
+};
+onlyMaxElement(arr);
