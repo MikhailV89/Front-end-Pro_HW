11a461624ddec7176aa11786dd004a4ec7247523
https://github.com/MikhailV89 <mvasilchenko89@gmail.com>
2024-09-19 15:04:24 +0300

    hw_20 ready

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,44 @@
 'use strict';
 
-const num1 = +prompt('Write first number');
-const num2 = +prompt('Write second number');
-const num3 = +prompt('Write third number');
+// Task #1
+const getFactorial = function (number) {
+  let factorial;
+  if (number === 0) {
+    factorial = 1;
+  } else {
+    factorial = number * getFactorial(number - 1);
+  }
 
-const result = (num1 + num2 + num3) / 3;
+  return factorial;
+};
 
-alert(`Cереднє арифметичне трьох чисел: ${result}`);
+console.log(getFactorial(3));
+
+// Task #2
+const pow = function (num, degree) {
+  let result;
+  if (degree === 0) {
+    result = 1;
+  } else {
+    result = num * pow(num, degree - 1);
+  }
+
+  return result;
+};
+
+console.log(pow(3, 7));
+
+// Task #3
+
+const recSum = function (num, count) {
+  let result;
+  if (count === 0) {
+    result = 1;
+  } else {
+    result = num + recSum(num + 1, count - 1);
+  }
+
+  return result;
+};
+
+console.log(recSum(7, 15));
