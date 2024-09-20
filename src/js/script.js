228c1388694f5b5c3640201474f54b4cf000227d
https://github.com/MikhailV89 <mvasilchenko89@gmail.com>
2024-09-20 12:28:09 +0300

    hw22 ready

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,33 @@
 'use strict';
 
-const num1 = +prompt('Write first number');
-const num2 = +prompt('Write second number');
-const num3 = +prompt('Write third number');
+function insertWord(str) {
+  const positions = ['start', 'middle', 'end'];
+  let currentIndex = 0;
 
-const result = (num1 + num2 + num3) / 3;
+  return function (word) {
+    if (!word) return str;
 
-alert(`Cереднє арифметичне трьох чисел: ${result}`);
+    word = ` ${word.trim()} `;
+    let result;
+
+    if (positions[currentIndex] === 'start') {
+      result = word.trim() + ' ' + str;
+    } else if (positions[currentIndex] === 'middle') {
+      const middle = Math.floor(str.length / 2);
+      result = str.slice(0, middle) + word + str.slice(middle);
+    } else {
+      result = str + word.trim();
+    }
+
+    currentIndex = (currentIndex + 1) % positions.length;
+    return result;
+  };
+}
+
+// Тестування
+const insert = insertWord('hello world');
+
+console.log(insert('Odesa'));
+console.log(insert('Odesa'));
+console.log(insert('Odesa'));
+console.log(insert('Odesa'));
