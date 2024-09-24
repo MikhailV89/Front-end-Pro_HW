c89a540d3332b9ef4138dc8727e8c5cbcd6d3909
https://github.com/MikhailV89 <mvasilchenko89@gmail.com>
2024-09-24 10:27:51 +0300

    hw23 ready

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,32 @@
 'use strict';
 
-const num1 = +prompt('Write first number');
-const num2 = +prompt('Write second number');
-const num3 = +prompt('Write third number');
+const arrFlat = function (...arr) {
+  if (arr.length > 1) {
+    throw new Error(
+      'Function accepts only 1 argument, too much arguments provided',
+    );
+  }
 
-const result = (num1 + num2 + num3) / 3;
+  const newArray = [];
+  const flatFunc = function (array) {
+    for (let i = 0; i < array.length; i += 1) {
+      if (Array.isArray(array[i])) {
+        flatFunc(array[i]);
+      } else {
+        newArray.push(array[i]);
+      }
+    }
+    return newArray;
+  };
+  return flatFunc(arr);
+};
 
-alert(`Cереднє арифметичне трьох чисел: ${result}`);
+const complexArray1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const complexArray = [
+  [1, 2, [3.1, 3.2, [3.21, 3.22]], 4],
+  [5, [6, [7, [8, 9]]]],
+  [[10, 11], 12, 13],
+];
+
+console.log(arrFlat(complexArray));
