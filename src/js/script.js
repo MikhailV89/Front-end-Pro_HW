9e85caa698a79c97d4dc6d4a22e56a027fd1e158
https://github.com/MikhailV89 <mvasilchenko89@gmail.com>
2024-10-17 10:21:04 +0300

    hw001 ready

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,40 @@
 'use strict';
 
-const num1 = +prompt('Write first number');
-const num2 = +prompt('Write second number');
-const num3 = +prompt('Write third number');
+class EventManager {
+  constructor() {
+    this.events = {};
+  }
 
-const result = (num1 + num2 + num3) / 3;
+  on(event, callback) {
+    if (!this.events[event]) {
+      this.events[event] = [];
+    }
+    this.events[event].push(callback);
+  }
 
-alert(`Cереднє арифметичне трьох чисел: ${result}`);
+  off(event, callback) {
+    if (!this.events[event]) return;
+
+    this.events[event] = this.events[event].filter((cb) => cb !== callback);
+  }
+
+  trigger(event, ...args) {
+    if (!this.events[event]) return;
+
+    this.events[event].forEach((callback) => {
+      callback.apply(this, args);
+    });
+  }
+}
+
+const manager = new EventManager();
+
+function greet(name) {
+  console.log(`Hello, ${name}!`);
+}
+
+manager.on('greet', greet);
+manager.trigger('greet', 'Mike');
+
+manager.off('greet', greet);
+manager.trigger('greet', 'Bill');
