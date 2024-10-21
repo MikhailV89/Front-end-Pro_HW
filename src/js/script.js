67d8222362ed3f7f53d0dc7c8361f5694da87e9a
https://github.com/MikhailV89 <mvasilchenko89@gmail.com>
2024-10-21 15:42:26 +0300

    hw002 ready

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,32 @@
 'use strict';
 
-const num1 = +prompt('Write first number');
-const num2 = +prompt('Write second number');
-const num3 = +prompt('Write third number');
+function BankAccount(initialBalance) {
+  let balance = initialBalance;
 
-const result = (num1 + num2 + num3) / 3;
+  this.deposit = function (amount) {
+    if (amount > 0) {
+      balance += amount;
+      console.log(`Deposit: +${amount}. New balance: ${balance}`);
+    } else {
+      console.log('Deposit amount must be greater than zero.');
+    }
+  };
 
-alert(`Cереднє арифметичне трьох чисел: ${result}`);
+  this.withdraw = function (amount) {
+    if (amount > 0 && amount <= balance) {
+      balance -= amount;
+      console.log(`Withdrawal: -${amount}. New balance: ${balance}`);
+    } else {
+      console.log('Insufficient funds or invalid amount.');
+    }
+  };
+
+  this.getBalance = function () {
+    return balance;
+  };
+}
+
+const myAccount = new BankAccount(150);
+myAccount.deposit(70);
+myAccount.withdraw(25);
+console.log(myAccount.getBalance());
